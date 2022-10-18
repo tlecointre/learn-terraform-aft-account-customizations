74ebbbda57663b467295bc6eefacdde93fa97a86
Tony Lecointre <tony@macbook-pro-de-tony.home>
2022-10-18 09:26:59 +0200

    Add custom role

--- sandbox/terraform/s3.tf
+++ sandbox/terraform/s3.tf
@@ -4,3 +4,25 @@ resource "aws_s3_bucket" "sandbox_bucket" {
   bucket = "aft-sandbox-${data.aws_caller_identity.current.account_id}"
   acl    = "private"
 }
+
+resource "aws_iam_role" "test_role" {
+  name = "aft_custom_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = {
+    tag-key = "tag-value"
+  }
+}
